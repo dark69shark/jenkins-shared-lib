a3f422978840a9d85b2ee168a35c5f26b936c367
ds <jestovip@gmail.com>
2023-10-22 22:43:58 +0300

    learning

---
+++ src/ru/sbrf/global/vars.groovy
@@ -0,0 +1,8 @@
+package ru.sbrf.global
+
+class vars {
+  final String STARTED = "STARTED"
+  final String SUCCESS = "SUCCESS"
+  final String FAILURE = "FAILURE"
+  final String ABORTED = "ABORTED"
+}
---
+++ vars/commonPipeline.groovy
@@ -0,0 +1,23 @@
+def call(Map args, Closure body) {
+    defaults = [
+        var1: 1,
+        var2: 2,
+        var3, "stfing"
+    ]
+    settings()
+    pipeline {
+        agent any
+        stages {
+            stage("commonStage1"){
+                steps {
+                    echo "stage1"
+                }
+            }
+            stage("Build"){
+                steps {
+                    body()
+                }
+            }
+        }
+    }
+}
